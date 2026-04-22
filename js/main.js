9fdc379786e7834eda1c3ef7953f023cf0298728
thaismota-tech <thaissantosdamota@gmail.com>
2026-04-22 10:59:19 +0200

    He finalizado el código del ejercicio 1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-// Ejercicio 2: Edad legal para votar
+// Ejercicio 2: Verificiar si usuario tiene la edad legal para votar
+function puedeVotar(edad) {
+
+console.log (edad)
+
+if (edad >= 18) {
+    return `Con la edad de ${edad} años puede votar`
+
+} if (edad <18) {
+    return `Con la edad de  ${edad} años no puede votar`
+}
+}
+console.log(puedeVotar(15))
+console.log(puedeVotar(18))
+console.log(puedeVotar(30))
+console.log(puedeVotar(17))
+
+// Ejercicio 3: Mayor de dos números
