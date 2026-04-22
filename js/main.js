7d1e29e555d512a57548d59e206e26e2ee86c07e
thaismota-tech <thaissantosdamota@gmail.com>
2026-04-22 14:38:59 +0200

    He finalizado el código del ejercicio 4

--- js/main.js
+++ js/main.js
@@ -30,3 +30,18 @@ console.log(mayorDeDos(45, 15))
 console.log(mayorDeDos(20, 30))
 console.log(mayorDeDos(15, 45))
 console.log(mayorDeDos(50, 1))
+
+// Ejercicio 4: Determinar si un número es divisible por 5
+function esDivisiblePorCinco(num) {
+console.log (num)
+if (num % 5 ===0) {
+      return true;
+} else {
+        return false;
+}
+}
+console.log(esDivisiblePorCinco (5))
+console.log(esDivisiblePorCinco (20))
+console.log(esDivisiblePorCinco (27))
+console.log(esDivisiblePorCinco (50))
+console.log(esDivisiblePorCinco (100))
