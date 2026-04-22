5cf28503c77a1c99875d1b739e428e24a473f197
thaismota-tech <thaissantosdamota@gmail.com>
2026-04-22 11:46:23 +0200

    He finalizado el código del ejercicio 3

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ console.log (edad)
 if (edad >= 18) {
     return `Con la edad de ${edad} años puede votar`
 
-} if (edad <18) {
+}else if (edad <18) {
     return `Con la edad de  ${edad} años no puede votar`
 }
 }
@@ -16,3 +16,17 @@ console.log(puedeVotar(30))
 console.log(puedeVotar(17))
 
 // Ejercicio 3: Mayor de dos números
+function mayorDeDos(num1, num2) {
+console.log (num1, num2)
+if (num1 > num2){
+    return `El número 1 ${num1} es mayor que el número 2 ${num2}`
+  
+}else{
+    return `El número 1 ${num1} no es mayor que el número 2 ${num2}`
+}
+}
+console.log(mayorDeDos(30, 15))
+console.log(mayorDeDos(45, 15))
+console.log(mayorDeDos(20, 30))
+console.log(mayorDeDos(15, 45))
+console.log(mayorDeDos(50, 1))
